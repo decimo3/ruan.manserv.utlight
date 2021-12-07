11f4117e3700ba834ca1258c76db3847c1fea5a3
Ruan faustino Camello Da Silva <ruanfaustinocamellodasiva@gmail.com>
2021-12-07 15:34:42 -0300

    simple component generation

--- website/index.js
+++ website/index.js
@@ -1,16 +1,20 @@
-function showPass () {
-  if (document.getElementById("palavra").getAttribute("type") == "password") {
-    document.getElementById("palavra").setAttribute("type", "text");
-    document.getElementById("mostrar").innerText = "O";
-  } else {
-    document.getElementById("palavra").setAttribute("type", "password");
-    document.getElementById("mostrar").innerText = "M";
-  }
+function criarElemento(pai, atributes) {
+    daddy = document.getElementById(pai);
+    elemento = document.createElement(atributes[0]);
+    index = 1;
+    if (atributes[1] == "innerHTML") {
+        elemento.innerHTML = atributes[2];
+        index += 2
+    }
+    while (index < atributes.length) {
+        elemento.setAttribute(atributes[index], atributes[index + 1]);
+        index += 2;
+    }
+    daddy.insertBefore(elemento, pai.nextSibling);
 }
-function criarElemento (pai, atributes) {
-  elemento = document.createElement(atributes[0]);
-  for (let index = 1; index < atributes.length; index+2) {
-    elemento.setAttribute(atributes[index],atributes[index+1]);
-  }
-  document.body.insertBefore(elemento, pai.nextSibling);
+
+function cabeca() {
+    cabecalho = ["h1", "innerHTML", "Manserv", "id", "cabecalho", "class", "filo"];
+    criarElemento("header", cabecalho);
 }
+cabeca();
