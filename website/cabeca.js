f88ca04f005721f134ecc9e03e04727fd67a56b5
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2021-12-09 19:55:43 -0300

    header automatically added

--- website/cabeca.js
+++
@@ -1,6 +0,0 @@
-function cabeca () {
-  // Elemento sequencia: "tipoElemento", "nomerDoAtributo", "valorDoAtributo" ...
-  header = document.getElementById("header");
-  elemento = ["h1", "id", "teste"];
-  criarElemento(header, elemento);
-}
--- website/index.js
+++ website/index.js
@@ -1,20 +1,20 @@
 function criarElemento(pai, atributes) {
-    daddy = document.getElementById(pai);
-    elemento = document.createElement(atributes[0]);
-    index = 1;
-    if (atributes[1] == "innerHTML") {
-        elemento.innerHTML = atributes[2];
-        index += 2
-    }
-    while (index < atributes.length) {
-        elemento.setAttribute(atributes[index], atributes[index + 1]);
-        index += 2;
-    }
-    daddy.insertBefore(elemento, pai.nextSibling);
+  daddy = document.getElementById(pai);
+  elemento = document.createElement(atributes[0]);
+  index = 1;
+  if (atributes[1] == "innerHTML") {
+    elemento.innerHTML = atributes[2];
+    index += 2
+  }
+  while (index < atributes.length) {
+    elemento.setAttribute(atributes[index], atributes[index + 1]);
+    index += 2;
+  }
+  daddy.insertBefore(elemento, pai.nextSibling);
 }
 
-function cabeca() {
-    cabecalho = ["h1", "innerHTML", "Manserv", "id", "cabecalho", "class", "filo"];
-    criarElemento("header", cabecalho);
+function inserirElementos() {
+  cabecalho = ["h1", "innerHTML", "Manserv", "id", "cabecalho", "class", "filo"];
+  criarElemento("header", cabecalho);
 }
-cabeca();
+inserirElementos();
