01e2682ef116a87c47ee7fd4ba7c484d48c0968f
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2021-12-12 22:20:02 -0300

    created team member

--- website/create.js
+++ website/create.js
@@ -1,22 +1,37 @@
 function criarComponente(index) {
-  // Elementos da div do label
-  divlabel = ["div", "class", "label", "id", ("divlabel" + index)];
-  criarElemento("create", divlabel);
-  // Elementos do label do input
-  formlabel = ["label", "innerHTML", ("Eletricista " + index), "for", ("eletricista" + index)];
-  criarElemento(("divlabel" + index), formlabel);
-  // Elementos do div input do formulário
-  divinput = ["div", "class", "input", "id", ("divinput" + index)];
-  criarElemento("create", divinput);
-  // Elementos do input do datalista
-  forminput = ["input", "list", ("listaEletricista" + index), "id", ("inputEletricista" + index)];
-  criarElemento(("divinput" + index), forminput);
-  // Elementos do datalist
-  datalist = ["datalist", "id", ("listaEletricista" + index)];
-  criarElemento(("inputEletricista" + index), datalist);
-  // Elemento do option do datalista
-  option = ["option", "innerHTML", "nome do eletricista", "value", "matricula"];
-  criarElemento(("listaEletricista" + index), option)
+  
+  // Cria uma div ṕara agrupar o componente
+  divcomp = ["div", "id", ("componente" + index), "class", "componente"];
+  criarElemento("create", divcomp);
+  for (let i = 1; i <= 2; i++) {
+    //div para agrupar elementos de eletricista
+    eletricista = ["div", "id", ("divEletricista" + index.toString() + i.toString())];
+    criarElemento(("componente" + index), eletricista);
+    // Elementos da div do label
+    divlabel = ["div", "class", "label", "id", ("divlabel" + index.toString() + i.toString())];
+    criarElemento(("divEletricista" + index.toString() + i.toString()), divlabel);
+    // Elementos do label do input
+    formlabel = ["label", "innerHTML", ("Eletricista " + i), "for", ("eletricista" + index.toString() + i.toString())];
+    criarElemento(("divlabel" + index.toString() + i.toString()), formlabel);
+    // Elementos do div input do formulário
+    divinput = ["div", "class", "input", "id", ("divinput" + index.toString() + i.toString())];
+    criarElemento(("divEletricista" + index.toString() + i.toString()), divinput);
+    // Elementos do input do datalista
+    forminput = ["input", "list", ("listaEletricista" + index.toString() + i.toString()), "id", ("inputEletricista" + index.toString() + i.toString())];
+    criarElemento(("divinput" + index.toString() + i.toString()), forminput);
+    // Elementos do datalist
+    datalist = ["datalist", "id", ("listaEletricista" + index.toString() + i.toString())];
+    criarElemento(("inputEletricista" + index.toString() + i.toString()), datalist);
+    // Elemento do option do datalista
+    option = ["option", "label", "Ruan Faustino Camello Da Silva", "value", "2258038"];
+    criarElemento(("listaEletricista" + index.toString() + i.toString()), option)
+    checkbox = ["input", "type", "checkbox", "value", true, "name", ("persistir" + index.toString() + i.toString())];
+    criarElemento(("divEletricista" + index.toString() + i.toString()), checkbox);
+    checklabel = ["label", "innerHTML", "Persistir mudança", "for", ("persistir" + index.toString() + i.toString())];
+    criarElemento(("divEletricista" + index.toString() + i.toString()), checklabel);
+  }
+  
+  
 }
 
 function criarElemento(pai, atributes) {
@@ -38,5 +53,6 @@ function inserirElementos() {
   eletricistas = ["option", "innerHTML", "Manserv", "value", "cabecalho"];
   criarElemento("eletricista", eletricistas);
 }
-inserirElementos();
+//inserirElementos();
+
 criarComponente(2);
