ff18bf518f7572edeb2e6bb011092e2574437989
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2021-12-20 21:14:57 -0300

    changed some variables name thats cause conflicts

--- website/index.js
+++ website/index.js
@@ -1,20 +1,20 @@
-function criarElemento(pai, atributes) {
+function criarCabecalho(pai, atributes) {
   daddy = document.getElementById(pai);
   elemento = document.createElement(atributes[0]);
-  index = 1;
+  indice = 1;
   if (atributes[1] == "innerHTML") {
     elemento.innerHTML = atributes[2];
-    index += 2
+    indice += 2
   }
   while (index < atributes.length) {
     elemento.setAttribute(atributes[index], atributes[index + 1]);
-    index += 2;
+    indice += 2;
   }
   daddy.insertBefore(elemento, pai.nextSibling);
 }
 
-function inserirElementos() {
+function inserirCabecalho() {
   cabecalho = ["h1", "innerHTML", "Manserv", "id", "cabecalho", "class", "filo"];
   criarElemento("header", cabecalho);
 }
-inserirElementos();
+inserirCabecalho();
